80851541df53e66e6836968a44a3a35854021928
takshak1206 <takshak@TAKSHAK-S.localdomain>
2026-01-29 18:56:44 +0000

    feat:Dashboard UI initialized

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,8 @@
 import { getSession } from "@/lib/auth/auth";
 import connectDB from "@/lib/db";
 import { Board } from "@/lib/models";
-import board from "@/lib/models/board";
 import { redirect } from "next/navigation";
+import KanbanBoard from "@/components/kanban-board";
 
 export default async function Dashboard() {
     const session = await getSession();
@@ -16,16 +16,20 @@ export default async function Dashboard() {
     const board=await Board.findOne({
         userId: session.user.id,
         name: "Job Hunt",
+    }).populate({
+        path: "columns",
     });
 
     console.log(board);
 
     return (
-        <div>
-            <div>
-                <div>
-                    <h1>{board?.name}</h1>
+        <div className="min-h-screen bg-white">
+            <div className="container mx-auto p-6">
+                <div className="mb-6">
+                    <h1 className="text-3xl font-bold text-black">{board?.name}</h1>
+                    <p className="text-gray-600">Track your job applications </p>
                 </div>
+                <KanbanBoard board={JSON.parse(JSON.stringify(board))} userId={session.user.id}/>
             </div>
         </div>
     )
