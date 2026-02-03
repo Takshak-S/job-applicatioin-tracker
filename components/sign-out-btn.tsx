e128e84c16eb81fb6ce7445fd334e869eaeb4c3f
takshak1206 <takshak@TAKSHAK-S.localdomain>
2026-02-03 14:04:21 +0000

    feat:Job application dialog box interaction and server action  added

--- components/sign-out-btn.tsx
+++ components/sign-out-btn.tsx
@@ -5,17 +5,20 @@ import { signOut } from "@/lib/auth/auth-client";
 import { useRouter } from "next/navigation";
 
 export default function SignOutBtn() {
-    const router=useRouter();
-    return (
-        <DropdownMenuItem className="hover:bg-primary/90" onClick={async ()=>{
-            const result=await signOut()
-            if(result.data) {
-                router.push('/sign-in');
-            } else {
-                alert("Failed to sign out. Please try again.");
-            }
-        }}>
-            Log Out
-        </DropdownMenuItem>
-    )
+  const router = useRouter();
+  return (
+    <DropdownMenuItem
+      className="hover:bg-primary/90"
+      onClick={async () => {
+        const result = await signOut();
+        if (result.data) {
+          router.push("/sign-in");
+        } else {
+          alert("Failed to sign out. Please try again.");
+        }
+      }}
+    >
+      Log Out
+    </DropdownMenuItem>
+  );
 }
